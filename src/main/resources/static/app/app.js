cad5959bccd4c5dc2b992bf5c0d0efff68a8a087
stephane gronowski <stephane.gronowski@gmail.com>
2016-09-24 21:28:52 +0200

    refacto reorga des view

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -10,10 +10,10 @@
 	app.config(function($routeProvider, $locationProvider) {
 
 		$routeProvider.when('/meetup', {
-			templateUrl : "app/meetup/list/listMeetup.html",
+			templateUrl : "app/meetup/view/list/listMeetup.html",
 			controller : "listMeetupController"
 		}).when('/meetup/:id', {
-			templateUrl : "app/meetup/meetup.html",
+			templateUrl : "app/meetup/view/meetup.html",
 			controller : "meetupController"
 		}).otherwise({
 			redirectTo : '/meetup'
