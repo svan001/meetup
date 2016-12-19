6cb065316ba3cc52855cf6ac354a98f13c963452
stephane <stephane.gronowski@gmail.com>
2016-12-19 13:28:34 +0100

    #28 #29 Refacto creation meetup en prenant en compte la validation du form

--- src/main/resources/static/app/meetup/component/add/addMeetup.js
+++ src/main/resources/static/app/meetup/component/add/addMeetup.js
@@ -7,26 +7,34 @@
         var ctrl = this;
 
         function resetForm() {
+            ctrl.datePickerOptions = {
+                minDate: new Date(),
+                showWeeks: true
+            };
+
             ctrl.meetupForm = {};
         }
+
+        ctrl.isFormValid = function () {
+            return ctrl.meetupForm.title && ctrl.meetupForm.date ?
+                true : false;
+        };
 
         ctrl.addMeetup = function () {
             var meetup = new Meetup(ctrl.meetupForm);
 
             meetup.$save().then(function () {
                 resetForm();
-                // $scope.meetups = Meetup.query();
-                ctrl.onSuccess();
+                ctrl.onSuccess({
+                    "meetup": meetup
+                });
             }, function (error) {
                 $log.error("création de meetup en erreur " + error)
             });
         }
 
         ctrl.$onInit = function () {
-            ctrl.datePickerOptions = {
-                minDate: new Date(),
-                showWeeks: true
-            };
+            resetForm();
         }
 
     };
